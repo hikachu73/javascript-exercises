255e007f034c7d7ebbb02ea976c825653a1bc808
Hikachu2004 <prabhavsthapit@gmail.com>
2026-04-26 06:43:38 +0000

    finished foundations/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let newWord = '';
+    if(num<0){
+        newWord = "ERROR";
+    }
+    for(let i=1;i<=num;i++){
+         newWord = newWord + word;
+    }
+    return newWord;
 };
-
+repeatString("hey",10);
+repeatString("hey",10);
+repeatString("hi",1);
+repeatString("bye",0);
+repeatString("",10);
+repeatString("hey",-1);
 // Do not edit below this line
 module.exports = repeatString;
