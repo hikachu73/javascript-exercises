927e27d6c1022083f8111ab00990aeab31578b02
Hikachu2004 <prabhavsthapit@gmail.com>
2026-04-27 07:12:49 +0000

    completed foundations/10_leapYears and 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsiusTemperature = (temperature-32)*(5/9);
+  return Math.round(celsiusTemperature*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheitTemperature = ((9/5)*temperature)+32;
+  return Math.round(fahrenheitTemperature*10)/10;
 };
-
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
